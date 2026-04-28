bb63d7314702a623c56f5d6270041e5de73502a9
Nitish <nitis@nitish.dev>
2026-04-28 17:56:38 +0530

    import fix

--- backend/chat_service/models/models.py
+++ backend/chat_service/models/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Text, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from database import Base
+from chat_service.models.database import Base
 
 
 # Association table for user channels
